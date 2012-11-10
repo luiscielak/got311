86f5e0b19d35877a31312537f61a431f32762dd6
Luis Cielak <luis.cielak@gmail.com>
2012-11-10 17:09:56 -0500

    update styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,65 @@
+/*	
+ *	got311.org 
+ *	main.js	v0.0.1
+ */
 
+
+// Zipstatic
+
+function is_int(value){ 
+  if((parseFloat(value) == parseInt(value)) && !isNaN(value)){
+    return true;
+  } else { 
+    return false;
+  } 
+}
+
+$(function() {
+
+  // Set up
+  $(".fancy-form div > div").hide();
+  var firstReveal = true;
+
+  $("#zip").keyup(function() {
+
+    // Cache 
+    var el = $(this);
+
+    // Did they type five integers?
+    if ((el.val().length == 5) && (is_int(el.val())))  {
+
+
+      // Call Ziptastic for information
+      $.ajax({
+        url: "http://zip.elevenbasetwo.com/v2",
+        cache: false,
+        dataType: "json",
+        type: "GET",
+        data: "zip=" + el.val(),
+        success: function(result, success) {
+
+          // Assign result to variables
+          $(".zip-error, .instructions").slideUp(200);
+          $("#city").val(result.city);
+          $("#state").val(result.state);      
+          $(".fancy-form div > div").slideDown();
+
+          // Write HTML div with location
+          document.getElementById("location-wrap").innerHTML=                
+            "<h3>"+result.city+", "+result.state+"</h3>";
+        },
+        error: function(result, success) {
+          $(".zip-error").slideDown(300);
+        }
+
+      });
+
+    } else if (el.val().length < 5) {
+
+      $(".zip-error").slideUp(200);
+
+    };
+
+  });
+
+}); // END DOM Ready
