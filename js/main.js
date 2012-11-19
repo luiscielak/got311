48fc1d216f9d80ade3fe5bb736a010940e78b25c
Luis Cielak <luis.cielak@gmail.com>
2012-11-19 00:25:50 -0500

    update theme.less

--- js/main.js
+++ js/main.js
@@ -4,7 +4,59 @@
  */
 
 
+// Flexsider
+// --------------------------------------------------
+
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationSpeed: 2000,
+    directionNav: true,
+    keyboard: true,
+    slideshow: true,
+    slideshowSpeed: 2000
+  });
+});
+
+$(document).ready(function(){
+  $(".scroll").click(function(event){
+    //prevent the default action for the click event
+    event.preventDefault();
+    
+    //get the full url - like mysitecom/index.htm#home
+    var full_url = this.href;
+    
+    //split the url by # and get the anchor target name - home in mysitecom/index.htm#home
+    var parts = full_url.split("#");
+    var trgt = parts[1];
+    
+    //find and remove exisiting active classes
+    $('.scrollnav').find('.active').removeClass('active');
+    
+    //add active class for just clicked link
+    $(this).addClass('active');
+
+    
+    //get the top offset of the target anchor
+    var target_offset = $("#"+trgt).offset();
+    var target_top = target_offset.top;
+    
+    //goto that anchor by setting the body scroll top to anchor top
+    $('html, body').animate({scrollTop:target_top}, 1000);
+  });
+});
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    // $('.scrollnav').find('.active').removeClass('active');
+  });
+});
+
+
+
+
 // Zipstatic
+// --------------------------------------------------
 
 function is_int(value){ 
   if((parseFloat(value) == parseInt(value)) && !isNaN(value)){
